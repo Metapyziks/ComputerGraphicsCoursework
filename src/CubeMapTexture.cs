3717be36dc963b5a3a0f4f7db0da4d5a663e9a8c
James King <metapyziks@gmail.com>
2013-02-18 02:21:55 +0000

    Added cubemap textures

---
+++ src/CubeMapTexture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGraphicsCoursework
+{
+    public class CubeMapTexture : Texture
+    {
+        public Bitmap Top { get; private set; }
+        public Bitmap Left { get; private set; }
+    
+        protected override void Load()
+        {
+ 	        throw new NotImplementedException();
+        }
+    }
+}
