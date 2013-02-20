ebbae358246d7c7ee0d2cb0d5352ac82a8b66f38
James King <metapyziks@gmail.com>
2013-02-20 01:08:24 +0000

    Some more cleanup

--- Properties/ComputerGraphicsCoursework.AssemblyInfo.cs
+++ Properties/ComputerGraphicsCoursework.AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ComputerGraphicsCoursework")]
+[assembly: AssemblyTitle("Computer Graphics Coursework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ComputerGraphicsCoursework")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © James King 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
